10b392d881231a9dcae808b1b22d8775be8f9213
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2017-02-27 23:20:16 +0100

    Use model class to represent the songs

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,20 +1,26 @@
 import Ember from 'ember';
 
+var Song = Ember.Object.extend({
+  title: '',
+  band: '',
+  rating: 0
+});
+
 export default Ember.Route.extend({
   model: function() {
-    var blackDog = Ember.Object.create({
+    var blackDog = Song.create({
       title: 'Black Dog',
       band: 'Led Zeppelin',
       rating: 3
     });
 
-    var yellowLedbetter = Ember.Object.create({
+    var yellowLedbetter = Song.create({
       title: 'Yellow Ledbetter',
       band: 'Pearl Jam',
       rating: 4
     });
 
-    var pretender = Ember.Object.create({
+    var pretender = Song.create({
       title: 'The Pretender',
       band: 'Foo Fighters',
       rating: 2
