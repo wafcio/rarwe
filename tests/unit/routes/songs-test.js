3ee29f096ccd6c86f7063c73d8cc5c06db9b8e15
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2017-02-28 10:45:02 +0100

    Use dedicated routes for bands and songs

--- tests/unit/routes/songs-test.js
+++ tests/unit/routes/songs-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:application', 'Unit | Route | application', {
+moduleFor('route:songs', 'Unit | Route | songs', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
