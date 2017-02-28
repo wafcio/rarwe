44b021de9388dec9f2e520d766771391e93506ff
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2017-02-28 13:06:15 +0100

    Use nested routes for bands and songs

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,14 +1,61 @@
 import Ember from 'ember';
 
 var Band = Ember.Object.extend({
-  name: ''
+  name: '',
+  songs: [],
+
+  // slug: Ember.computed('name', function() {
+  //   return this.get('name').dasherize();
+  // }),
+
+  slug: function() {
+    return this.get('name').dasherize();
+  }.property('name'),
+
+  // site: Ember.computed('slug', 'language', function() {
+  //   return 'http://bands.com/' + this.get('slug') + '.' + this.get('language');
+  // })
+
+  site: function() {
+    return 'http://bands.com/' + this.get('slug') + '.' + this.get('language');
+  }.property('slug', 'language')
 });
+
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
+})
 
 export default Ember.Route.extend({
   model: function() {
-    var ledZeppelin = Band.create({ name: 'LedZeppelin' });
-    var pearlJam = Band.create({ name: 'Pearl Jam' });
-    var fooFighters = Band.create({ name: 'Foo Fighters' });
+    var blackDog = Song.create({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+    });
+
+    var yellowLedbetter = Song.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+
+    var pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    var daughter = Song.create({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5
+    });
+
+    var ledZeppelin = Band.create({ name: 'LedZeppelin', songs: [blackDog] });
+    var pearlJam = Band.create({ name: 'Pearl Jam', songs: [yellowLedbetter, daughter] });
+    var fooFighters = Band.create({ name: 'Foo Fighters', songs: [pretender] });
 
     return [ledZeppelin, pearlJam, fooFighters];
   }
--- tests/unit/routes/bands/band-test.js
+++ tests/unit/routes/bands/band-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:songs', 'Unit | Route | songs', {
+moduleFor('route:bands/band', 'Unit | Route | bands/band', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
